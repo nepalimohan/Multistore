adf8ea60bb8adbce4eb963f5533a43df8114a8dd
nepalimohan <nepalimohan@outlook.com>
2023-01-07 22:27:05 -0800

    ProductModelsDone

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from product.models import Category, SubCategory, Product
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+@admin.register(SubCategory)
+class SubCategory(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price','subcategory', 'size', 'stock', 'description', 'is_featured']
+    
