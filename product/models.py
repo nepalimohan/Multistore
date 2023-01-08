b1666293f5106e77c6112ead7b11c39eb9062e14
nepalimohan <nepalimohan@outlook.com>
2023-01-07 22:14:33 -0800

    ProductModelCreated

--- product/models.py
+++ product/models.py
@@ -1,17 +1,47 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class Product(models.Model):
+# Create your models here.
+class Category(models.Model):
+    category_choices = (
+        ('Mens','Mens'),
+        ('Womens','Womens'),
+        ('Unisex','Unisex'),
+    )
+    name = models.CharField(max_length=50, choices=category_choices)
     
-#     category_choices = {
-#         'Jeans': 'Jeans',
-#         'Shoes': 'Shoes',
-#         'Socks': 'Socks',
-#     }
+    def __str__(self):
+        return self.name
+    
+class SubCategory(models.Model):
+    subcategory_choices = (
+        ('Jeans','Jeans'),
+        ('Shoes','Shoes'),
+        ('Socks','Socks'),
+        ('Hoddie','Hoodie'),
+    )
+    name = models.CharField(max_length=50, choices=subcategory_choices)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    sizes_choices = (
+        ('S','Small'),
+        ('M','Medium'),
+        ('L','Large'),
+        ('XL','Extra Large'),
+        ('XXL','Extra Extra Large'),
+        )
     
     
-#     name = models.CharField(max_length=100)
-#     price = models.PositiveIntegerField()
-#     description = models.TextField()
-#     category = models.CharField(max_length=100, choices=category_choices)
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    size = models.CharField(max_length=10, choices=sizes_choices, null=True, blank=True)
+    stock = models.PositiveIntegerField()
+    description = models.TextField()
+    is_featured = models.BooleanField(default=False)
     
+    def __str__(self):
+        return self.name
