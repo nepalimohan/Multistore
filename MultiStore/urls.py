db15ac5b4d60948ff001308468bd49270747757c
nepalimohan <nepalimohan@outlook.com>
2023-01-08 00:01:22 -0800

    Changes Mafe

--- MultiStore/urls.py
+++ MultiStore/urls.py
@@ -20,6 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('account/', include('account.urls')),
-    path('product/', include('product.urls')),  
+    path('account/', include('account.urls', namespace='account')),
+    path('product/', include('product.urls', namespace='product')),  
 ]
