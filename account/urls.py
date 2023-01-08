2440d83eb4437a8eab5a37b06cac9f621c74ef34
nepalimohan <nepalimohan@outlook.com>
2023-01-07 23:44:20 -0800

    NameSpaceUsed

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from account.views import login, register
 
-
+app_name = 'account'
 
 urlpatterns = [
     path('login/', login, name='login'),
--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'product'
+
 urlpatterns = [
     path('shop/', views.shop, name='shop'),
 ]
