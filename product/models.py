2c3a0e00928c839ef3625539b44b38522fef4426
nepalimohan <nepalimohan@outlook.com>
2023-01-09 22:01:33 -0800

    CustomerModelCreated

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -45,3 +46,28 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Customer(models.Model):
+    gender_choices = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+    
+    city_choices = (
+        ('Kathmandu','Kathmandu'),
+        ('Chitwan','Chitwan'),
+        ('Pokhara','Pokhara'),
+    )
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone = models.PositiveIntegerField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50,choices=city_choices)
+    gender = models.CharField(max_length=10, choices=gender_choices)
+    
+    def __str__(self):
+        return self.first_name
+    
