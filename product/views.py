0189d3aa9adc11be04a1c7fc35d0774a85234ce2
nepalimohan <nepalimohan@outlook.com>
2023-01-17 21:12:47 +0545

    changes

--- product/views.py
+++ product/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from . import models
 
 # Create your views here.
 def products(request, id):
     product = models.Product.objects.filter(subcategory__id=id)
     print(product)
+    context = {
+        'product': product,
+    }
+    
+    return HttpResponse("Dynamic")
 
 
 def shop(request):
