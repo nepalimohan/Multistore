89c1316d66f8a90d58443b8908820d44ba15817b
nepalimohan <nepalimohan@outlook.com>
2023-01-24 21:46:11 +0545

    Models Updated

--- product/models.py
+++ product/models.py
@@ -3,12 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
-    category_choices = (
-        ('Mens','Mens'),
-        ('Womens','Womens'),
-        ('Unisex','Unisex'),
-    )
-    name = models.CharField(max_length=50, choices=category_choices)
+    name = models.CharField(max_length=50, blank=True, null=True)
 
     parent = models.ForeignKey('self', on_delete=models.CASCADE,null=True, blank= True, related_name='children')
     
@@ -16,12 +11,7 @@ class Category(models.Model):
         return self.name
     
 class Subcategory(models.Model):
-    name = models.CharField(max_length=50, choices=[
-        ('Jeans','Jeans'),
-        ('Shoes','Shoes'),
-        ('Socks','Socks'),
-        ('Hoddie','Hoodie'),
-    ])
+    name = models.CharField(max_length=50, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
     class Meta:
         verbose_name = 'SubCategory'
@@ -36,23 +26,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='uploads/')
     description = models.TextField(blank=True)
-    stock = models.PositiveIntegerField()
-    SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-        ('XL', 'Extra Large'),
-        ('XXL', 'Extra Extra Large'),
-    )
-    SHOE_SIZES = (
-        (39, 39),
-        (40, 40),
-        (41, 41),
-        (42, 42),
-        (42, 42),
-    )    
-    clothes_size = models.CharField(max_length=3, choices=SIZES ,blank=True, null=True)
-    shoe_size = models.PositiveIntegerField(choices=SHOE_SIZES ,blank=True, null=True)
+    stock = models.PositiveIntegerField()  
+    clothes_size = models.CharField(max_length=100, blank=True, null=True)
+    shoe_size = models.PositiveIntegerField(blank=True, null=True)
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
@@ -60,19 +36,13 @@ class Product(models.Model):
     
     
 class Customer(models.Model):
-    city_choices = (
-        ('Kathmandu','Kathmandu'),
-        ('Chitwan','Chitwan'),
-        ('Pokhara','Pokhara'),
-    )
-    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     phone = models.PositiveIntegerField()
     address = models.CharField(max_length=100)
-    city = models.CharField(max_length=50,choices=city_choices)
+    city = models.CharField(max_length=50, blank=True, null=False)
     
     
     def __str__(self):
